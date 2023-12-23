c1992ad848f1545b2aa98b5c0088bd197d51ab96
Tio Rambe Putra <tiorambe1104@gmail.com>
2023-12-24 03:27:56 +0700

    Init commit and add layout with navbar

---
+++ src/components/ActionButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <button v-on:click='a'>Button</button>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="flex justify-between navbar bg-base-100">
+  <a class="text-xl btn btn-ghost" href='/'>Tio Rambe P.</a>
+  <ul class="px-1 menu menu-horizontal">
+    <li><a href="">About Me</a></li>
+    <li><a href="">Blog</a></li>
+    <li><a href="">Contact</a></li>
+  </ul>
+</div>
+</template>
