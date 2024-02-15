079ebf7e08df0887ddd5458db03a8820c119fe1d
Tio Rambe Putra <tiorambe1104@gmail.com>
2024-02-15 19:30:19 +0700

    Created the footer

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,12 @@
   </header>
 
   <RouterView />
+
+  <TheFooter />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
 import TheNavbar from './components/Shared/TheNavbar.vue'
+import TheFooter from './components/Shared/TheFooter.vue'
 </script>
---
+++ src/components/Shared/TheFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="p-10 text-black rounded bg-primary footer footer-center">
+    <nav class="grid grid-flow-col gap-4">
+      <p>Contact me by:</p>
+    </nav>
+    <nav>
+      <div class="grid grid-flow-col gap-4">
+        <a><IconWhatsapp /></a>
+        <a><IconLinkedin /></a>
+        <a><IconInstagram /></a>
+      </div>
+    </nav>
+    <aside>
+      <p>Created by: Tio Rambe Putra</p>
+    </aside>
+  </footer>
+</template>
+
+<script setup>
+import IconInstagram from '../Icon/IconInstagram.vue'
+import IconLinkedin from '../Icon/IconLinkedin.vue'
+import IconWhatsapp from '../Icon/IconWhatsapp.vue'
+</script>
