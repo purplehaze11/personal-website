f46dcb05cf89de812f993e49cca63dfa247fa643
Tio Rambe Putra <tiorambe1104@gmail.com>
2024-02-15 18:36:40 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <!-- put navbar here  -->
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <!-- content -->
+  </main>
+</template>
